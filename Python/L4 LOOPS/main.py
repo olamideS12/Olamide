751b73157bc4dc352ad81b3247e3b9ee55262b08
olamideS12 <haddiyatafeez2@gmail.com>
2025-03-01 17:57:02 +0100

    loops

--- Python/L4 LOOPS/main.py
+++ Python/L4 LOOPS/main.py
@@ -10,3 +10,8 @@ accessories =["brush","comb","earings"]
 for x in adj:
     for y in accessories:
         print(x,y)
+
+for i in range(1,5):
+    for j in range(i):
+        print("*", end="")
+    print()
