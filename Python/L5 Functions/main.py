627e8f82e6509eb5c7dc3561d38bb92cb16ed926
olamideS12 <haddiyatafeez2@gmail.com>
2025-03-02 17:47:05 +0100

    functions

---
+++ Python/L5 Functions/main.py
@@ -0,0 +1,25 @@
+def mynameis():
+    print("Haddiyat Olamide Afeez")
+
+def favcolor(color):
+    print("fav color is", color)
+
+mynameis()
+favcolor("purple")
+
+def sum(x,y):
+    return x+y
+
+def division(num1,num2):
+    return num1/num2
+
+def multiplication(a,b):
+    return a*b
+
+def subtraction(x,y):
+    return x-y
+
+print(sum(4,6))
+print(division(16,4))
+print(multiplication(4,3))
+print(subtraction(10,4))
