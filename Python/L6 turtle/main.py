08b289ba352bc1584a372b2e588307ad283d004f
olamideS12 <haddiyatafeez2@gmail.com>
2025-03-15 17:58:40 +0100

    turtle

---
+++ Python/L6 turtle/main.py
@@ -0,0 +1,21 @@
+import turtle
+sc= turtle.Screen()
+sc.bgcolor("purple")
+sc.setup(300,300)
+sc.title("draw")
+
+t=turtle.Turtle()
+#t.forward(120)
+#t.left(90)
+#t.forward(120)
+#t.left(90)
+#t.forward(120)
+#t.left(90)
+#t.forward(120)
+
+for i in range(4):
+    t.forward(120)
+    t.left(90)
+
+
+turtle.done()
